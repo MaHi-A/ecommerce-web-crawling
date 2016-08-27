ad8dce8795ccbf8c6dd85e631ddc37e90e42fbcf
Mahesh <anethalamahesh@gmail.com>
2016-08-28 01:41:40 +0530

    Integrated devise and done UI cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def layout_by_resource
+    if signed_in?
+      'application'
+    else
+      'home'
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|parameter| parameter.permit(:email, :password)}
+  end
 end
