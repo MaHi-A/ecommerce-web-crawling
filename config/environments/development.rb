85e32d4d518afed31f508974d44ea9972e3067fd
Mahesh <anethalamahesh@gmail.com>
2016-08-28 22:44:44 +0530

    email setup done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -42,6 +42,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -51,4 +53,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   address: 'smtp.gmail.com',
+   port: 587,
+   user_name: 'your gmail ID',
+   password: 'your password',
+   authentication: "plain",
+   enable_starttls_auto: true
+  }
 end
