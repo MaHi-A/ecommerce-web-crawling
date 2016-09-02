fdba8238bf6339eefdc9393289621736b888190b
Mahesh <anethalamahesh@gmail.com>
2016-09-02 15:40:12 +0530

    adding heroku keys for sending mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    address: 'smtp.gmail.com',
    port: 587,
-   user_name: 'your gmail ID',
-   password: 'your password',
+   user_name: ENV['GOOGLE_USERNAME'],
+   password: ENV['GOOGLE_PASSWORD'],
    authentication: "plain",
    enable_starttls_auto: true
   }
