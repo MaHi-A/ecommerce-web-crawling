d76d5cd5b1f8d6344f95844b6d7eafad5c1eb94a
Mahesh <anethalamahesh@gmail.com>
2016-09-03 11:22:31 +0530

    ui cleanup

--- app/decorators/activities_decorator.rb
+++ app/decorators/activities_decorator.rb
@@ -1,2 +1,7 @@
 class ActivitiesDecorator < Draper::CollectionDecorator
+  def crawled_products_count
+    self.each { |activity, sum| sum + activity.crawl.products.length }
+
+    # <%= @activities.crawled_products_count %>
+  end
 end
